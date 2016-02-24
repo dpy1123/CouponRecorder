80f8059100fb5525bf282a8a7807fe439afae136
dd <dpy1123@126.com>
2016-02-24 16:11:16 +0800

    add dislike log

--- smzdm.js
+++ smzdm.js
@@ -1,21 +1,44 @@
 function insertCRFunc(){
 	
+	function getItemId(text){//从text中提取itemId
+		var itemId = NaN;
+		var regResult = text.match(/[1-9][0-9]*/g);//匹配数字部分
+		if(regResult.length > 0){
+			itemId = regResult[regResult.length-1];
+		}
+		return itemId;
+	}
+
+
 	//因为list元素会随着浏览动态添加，因此使用on绑定事件
 	$('body > section > div.leftWrap').on('click', 'div.list > div.listTitle > h4 > a', function(){
 		var url = this.href;//http://www.smzdm.com/p/6016706/
-		var regResult = url.match(/[1-9][0-9]*/g);//匹配数字部分
-		if(regResult.length > 0){
-			var itemId = regResult[0];
+		var itemId = getItemId(url);
+		if(!isNaN(itemId)){
 			sendUserAction(null, itemId, 'view', 'smzdm');
 		} 
+	}).on('mouseover', 'div.list > div.listTitle > h4 > a' , function(){
+		//console.log('hover')
+		var itemId = getItemId(this.href);
+		if($('#popup_'+itemId).length == 0){//只append一次
+			var popup = document.createElement("span");
+			popup.id = 'popup_'+itemId;
+			popup.style.float = 'right';
+			popup.style.cursor = 'pointer';
+			popup.innerText = 'dislike';
+			popup.onclick = function(){
+				sendUserAction(null, itemId, 'dislike', 'smzdm');
+				$(this).remove();
+			}
+			$(this).parent().append(popup);
+		}
 	});
 
 	
 	$('body > section > div.leftWrap').on('click', 'div.list > a.picLeft', function(){
 		var url = this.href;//http://www.smzdm.com/p/6016706/
-		var regResult = url.match(/[1-9][0-9]*/g);//匹配数字部分
-		if(regResult.length > 0){
-			var itemId = regResult[0];
+		var itemId = getItemId(url);
+		if(!isNaN(itemId)){
 			sendUserAction(null, itemId, 'view', 'smzdm');
 		}
 	});
@@ -24,9 +47,8 @@ function insertCRFunc(){
 	
 	$('body > section > div.leftWrap').on('click', 'div.list > div.listRight > div.lrBot > div.botPart > div.buy > a', function(){
 		var url = $(this).parents('div.list').attr('articleid');//"3_6017116"
-		var regResult = url.match(/[1-9][0-9]*/g);//匹配数字部分
-		if(regResult.length > 0){
-			var itemId = regResult[regResult.length-1];
+		var itemId = getItemId(url);
+		if(!isNaN(itemId)){
 			sendUserAction(null, itemId, 'buy', 'smzdm');
 		}
 	});
